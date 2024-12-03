ffb2937ae40988281630609e844e64c946182e09
EvandroKaibara <yujievandro@gmail.com>
2024-12-03 12:23:20 -0300

    Bloco 5

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-function tocaSom(idElementoAudio){   
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){   
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null && elemento.localName != 'audio'){
+        alert('Elemento não encontrado');
+    }
+    else{
+        elemento.play();
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while(contador < listaDeTeclas.length){
-    const tecla = listaDeTeclas[contador];
+for(let i=0;i<listaDeTeclas.length;i++){
+    const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    console.log(instrumento);
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
-    };
-    contador++;
-    console.log(contador);
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
